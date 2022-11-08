0342c65f1406fcdf42bd05040a454c3f669a9b5a
Cx330Lei <93487102+Cx330Lei@users.noreply.github.com>
2022-11-08 20:04:22 +0700

    Update del_user.php

--- giaoVien/del_user.php
+++ giaoVien/del_user.php
@@ -7,6 +7,14 @@
         exit();
     }
     $id = $_GET['id'];
+    $s = "select * from `infusers` WHERE id='$id'";
+    $q=mysqli_query($conn,$s);
+    $r=mysqli_fetch_array($q);
+    //kiểm tra người được chỉnh sửa là sinh viên hay giáo viên
+    if($r['role'] == 1)
+    {
+        header("location:manage_user.php");
+    } 
     $sql = "DELETE FROM `infusers` WHERE id='$id'";
 	$query = mysqli_query($conn, $sql);
 	header("location:manage_user.php");
